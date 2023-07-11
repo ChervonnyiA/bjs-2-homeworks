06a674e552769c21ca5239ce6c3296846c71838a
Anton Chervonny <anton75877587@mail.ru>
2023-07-12 00:05:32 +0800

    Задание 3 выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length) {
+    return arr1.every((element, i) => element === arr2[i]);
+  } else {
+    return false;
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, element, index, arr) => {
+    acc += element;
+    if (index === (arr.length - 1)) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
 }
